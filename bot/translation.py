7633bdd898cfbeb2a17434bf9fab05a8ff932d31
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2021-08-18 14:23:52 +0530

    Initial commit

---
+++ bot/translation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @AlbertEinsteinTG
+
+class Translation(object):
+    
+    START_TEXT = """<b>
+Sorry, This Bot Can Only Be Used In @MOVIERG_NGY ..!
+For Queries, @NGYNY!
+</i>"""    
+    
+    HELP_TEXT = """
+Sorry, I Can't Help You!
+"""
+    
+    ABOUT_TEXT = """
+    Nothing Great About Me..!
+    """
