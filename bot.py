ea65f4b5606d177f01e1d2a8da4be7f07cc725fd
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-09-02 20:28:03 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,39 +2,41 @@
 # -*- coding: utf-8 -*-
 # (c) @SpEcHIDe
 
-from pyrogram import Client, __version__
+from pyrogram import Client
+from pyrogram.types import BotCommand
 
-# ⬇️ FIXED: use absolute imports instead of relative
-from config import API_HASH, APP_ID, LOGGER, BOT_TOKEN
-from user import User
+from .config import API_HASH, APP_ID, LOGGER, BOT_TOKEN
+from .user import User
 
 
 class Bot(Client):
-    USER: User = None
-    USER_ID: int = None
-
     def __init__(self):
         super().__init__(
             "bot",
             api_hash=API_HASH,
             api_id=APP_ID,
-            plugins={
-                "root": "bot/plugins"
-            },
+            plugins={"root": "bot/plugins"},
             workers=200,
             bot_token=BOT_TOKEN,
-            sleep_threshold=10
+            sleep_threshold=10,
         )
         self.LOGGER = LOGGER
+        self.user: User = None
+        self.user_id: int = None
 
     async def start(self):
         await super().start()
         bot_details = await self.get_me()
         self.set_parse_mode("html")
-        self.LOGGER(__name__).info(
-            f"@{bot_details.username} started!"
+        self.LOGGER(__name__).info(f"@{bot_details.username} started!")
+        self.user, self.user_id = await User().start()
+        await self.set_bot_commands(
+            [
+                BotCommand("start", "Starts the bot"),
+                BotCommand("help", "Shows the help message"),
+                BotCommand("about", "Shows the about message"),
+            ]
         )
-        self.USER, self.USER_ID = await User().start()
 
     async def stop(self, *args):
         await super().stop()
